5b08adcfec4e161cf09aef02db6fb4c0230cd7d5
abeljlim <subrosian@hotmail.com>
2023-04-03 20:54:34 -0700

    ESLint use on head.js

--- head.js
+++ head.js
@@ -9,9 +9,9 @@ const assertEqual = function(actual, expected) {
 
 const head = function(arr) {
   return arr[0];
-}
+};
 
-assertEqual(head([5,6,7]), 5);
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["The only element"]), "The only element");
 assertEqual(head([]), undefined);
