82f3249772552146165c21b34406521c3bdaf67c
abeljlim <subrosian@hotmail.com>
2023-04-04 19:53:49 -0700

    use ESLint on eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,7 +6,7 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   // if the arrays' lengths differ, then return false
   if (arr1.length !== arr2.length) {
     return false;
@@ -14,7 +14,7 @@ const eqArrays = function (arr1, arr2) {
 
   // iterate through arr1 (and arr2, which has an equal length) and check for inequality of each element of arr1 to arr2's corresponding element, and return false if at least one inequality is found
   for (let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
@@ -22,7 +22,7 @@ const eqArrays = function (arr1, arr2) {
   // It's okay for eqArrays to not return true for nested arrays or arrays of objects that are identical. We will save this "deeper" problem for another day.
   
   return true; // No inequalities found
-}
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
