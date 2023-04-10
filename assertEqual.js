a750c332fd65f579c43a107e1c7b55578e684139
abeljlim <subrosian@hotmail.com>
2023-04-10 15:44:16 -0700

    tinkering code to find out how require works

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,16 @@
 // FUNCTION IMPLEMENTATION
+const aFunction = function() {console.log("aFunction was called")};
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
   }
+  aFunction();
 };
+
+const variableLimitedToFile = "variableLimitedToFile string";
+console.log(aFunction());
+console.log("variableLimitedToFile: ",variableLimitedToFile);
 
 module.exports = assertEqual;
--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,9 @@
 const head = require('../head');
 const assertEqual = require('../assertEqual'); // Why doesn't the code already run so the function is already declared?
+// const assertEqual2 = require('../assertEqual');
+// console.log(variableLimitedToFile); // variableLimitedToFile being within the scope of the file at '..assertEqual'
 
+// assertEqual2([5], 5);
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["The only element"]), "The only element");
