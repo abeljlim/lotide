3c75988fb2bd6e58bc910f5f33f6e6ce52a1c00f
abeljlim <subrosian@hotmail.com>
2023-04-10 19:08:06 -0700

    CRLF on some files, and index.js that consists of utility functions

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  head: require('./head'),
+  tail: require('./tail'),
+  middle: require('./middle'),
+  eqArrays: require('./eqArrays'),
+  assertArraysEqual: require('./assertArraysEqual'),
+  assertEqual: require('./assertEqual')
+};
