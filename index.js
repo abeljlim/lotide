95245189bb53b20b0e9b9e725616609fc5662344
abeljlim <subrosian@hotmail.com>
2023-04-10 21:11:41 -0700

    Attempt to add eqObjects.js to a require call but finding that it results in running a lot of assertion test code so not doing it

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const middle = require('./middle');
 const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
+// const eqObjects = require('./eqObjects'); // not adding this utility function or any others without separate test files because it results in a lot of assertion code running
 
 module.exports = {
   head,
@@ -11,5 +12,6 @@ module.exports = {
   middle,
   eqArrays,
   assertArraysEqual,
-  assertEqual
+  assertEqual,
+  // eqObjects
 };
