c1a2c68f891d204973520a0e0cfd4786dcdc70eb
abeljlim <subrosian@hotmail.com>
2023-04-10 18:34:27 -0700

    Refactoring tail and middle with Mocha and Chai tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,27 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
-assertEqual(words.length, 3);
-assertEqual(tail(words).length, 2);
-assertEqual(tail(words)[0], "Lighthouse");
-assertEqual(tail(words)[1], "Labs");
+describe(`#tail (where words points to ["Yo Yo", "Lighthouse", "Labs"])`, () => {
+  it(`returns 3 for words.length`, () => {
+    assert.deepEqual(words.length, 3);
+  });
+  it(`returns 2 for tail(words).length`, () => {
+    assert.deepEqual(tail(words).length, 2);
+  });
+  it(`returns "Lighthouse" for tail(words)[0]`, () => {
+    assert.deepEqual(tail(words)[0], "Lighthouse");
+  });
+  it(`returns "Labs for tail(words)[1]`, () => {
+    assert.deepEqual(tail(words)[1], "Labs");
+  });
 
-// Testing "An array with only one element should yield an empty array for its tail"
-assertEqual(tail(["Heya"])[0], undefined);
-
-// Testing "An empty array should yield an empty array for its tail"
-assertEqual(tail([])[0], undefined);
+  it(`An array ["Heya"] that has only one element should yield an empty array for its tail`, () => {
+    assert.deepEqual(tail(["Heya"])[0], undefined);
+  });
+  it(`An empty array should yield an empty array for its tail`, () => {
+    assert.deepEqual(tail([])[0], undefined);
+  });
+});
